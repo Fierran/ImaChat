fa1addfdb4d51c0a77e9f0bd308685a005db7436
Fierran <ferransuabc@gmail.com>
2024-11-10 16:30:39 -0800

    pantalla de conversaciones casi terminada

--- assets/datos/ConversacionD.js
+++ assets/datos/ConversacionD.js
@@ -1,3 +1,19 @@
 export const ConversacionD = {
-    Saludo: require('../DetallesEmocion/feliz.txt'),
+    Saludo: require('../DetallesConversacion/saludo.txt'),
+    Clima: require('../DetallesConversacion/clima.txt'),
+    Peliculas: require('../DetallesConversacion/pelis.txt'),
+    Musica: require('../DetallesConversacion/musica.txt'),
+    Libros: require('../DetallesConversacion/libros.txt'),
+    Pasatiempos: require('../DetallesConversacion/pasatiempos.txt'),
+    Comida: require('../DetallesConversacion/comida.txt'),
+    Animales: require('../DetallesConversacion/animales.txt'),
+    Vacaciones: require('../DetallesConversacion/vacas.txt'),
+    Deportes: require('../DetallesConversacion/deportes.txt'),
+    Tecnologia: require('../DetallesConversacion/tec.txt'),
+    Videojuegos: require('../DetallesConversacion/videojuegos.txt'),
+    Noticias: require('../DetallesConversacion/noticias.txt'),
+    Arte: require('../DetallesConversacion/arte.txt'),
+    Planes: require('../DetallesConversacion/planes.txt'),
+    Metas: require('../DetallesConversacion/metas.txt'),
+
 }
--- subscreens/Conversacion.jsx
+++ subscreens/Conversacion.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { Asset } from 'expo-asset';
 import * as FileSystem from 'expo-file-system';
 import { conversaciones } from '../assets/datos/Conversacion';
+import { ConversacionD } from '../assets/datos/ConversacionD';
 
 import { EmotionDetail } from './EmotionDetail';
 
@@ -13,31 +14,12 @@ export function Conversacion() {
     const [selectedEmotion, setSelectedEmotion] = useState(null);
     const [emotionInfo, setEmotionInfo] = useState('');
 
-    const emociones = [
-      { id: '1', label: 'Feliz', uri: "https://w7.pngwing.com/pngs/528/425/png-transparent-smile-emoji-happy-happiness-happy-face-yellow-smiling-message-social-networks-whatsapp.png", filename: 'feliz.txt' },
-      { id: '2', label: 'Triste', uri: "https://c0.klipartz.com/pngpicture/969/1005/gratis-png-emoji-triste-arte-cara-con-lagrimas-de-alegria-emoji-llorando-emoticon-llorar-thumbnail.png" },
-      { id: '3', label: 'Enojado', uri: "https://w7.pngwing.com/pngs/666/250/png-transparent-angry-emoji-illustration-emoji-anger-emoticon-iphone-angry-emoji-orange-computer-wallpaper-smiley.png" },
-      { id: '4', label: 'Sorprendido', uri: "https://e7.pngegg.com/pngimages/233/277/png-clipart-emoji-emoticon-smiley-iphone-computer-icons-be-surprised-sphere-circle.png" },
-      { id: '5', label: 'Sorprendido', uri: "https://e7.pngegg.com/pngimages/233/277/png-clipart-emoji-emoticon-smiley-iphone-computer-icons-be-surprised-sphere-circle.png" },
-      { id: '6', label: 'Sorprendido', uri: "https://e7.pngegg.com/pngimages/233/277/png-clipart-emoji-emoticon-smiley-iphone-computer-icons-be-surprised-sphere-circle.png" },
-      { id: '7', label: 'Sorprendido', uri: "https://e7.pngegg.com/pngimages/233/277/png-clipart-emoji-emoticon-smiley-iphone-computer-icons-be-surprised-sphere-circle.png" },
-      { id: '8', label: 'Sorprendido', uri: "https://e7.pngegg.com/pngimages/233/277/png-clipart-emoji-emoticon-smiley-iphone-computer-icons-be-surprised-sphere-circle.png" },
-      { id: '9', label: 'Sorprendido', uri: "https://e7.pngegg.com/pngimages/233/277/png-clipart-emoji-emoticon-smiley-iphone-computer-icons-be-surprised-sphere-circle.png" },
-      { id: '10', label: 'Sorprendido', uri: "https://e7.pngegg.com/pngimages/233/277/png-clipart-emoji-emoticon-smiley-iphone-computer-icons-be-surprised-sphere-circle.png" },
-      { id: '11', label: 'Sorprendido', uri: "https://e7.pngegg.com/pngimages/233/277/png-clipart-emoji-emoticon-smiley-iphone-computer-icons-be-surprised-sphere-circle.png" },
-      // Agrega más emociones según lo necesario
-  ];
-
-  const emotionFiles = {
-    Feliz: require('../assets/DetallesEmocion/feliz.txt'),
-    Triste: require('../assets/DetallesEmocion/triste.txt')};
-
   const handleEmotionSelect = async (emotion) => {
     setSelectedEmotion(emotion);
 
     try {
       // Cargar el archivo de texto correspondiente a la emoción seleccionada
-      const asset = Asset.fromModule(emotionFiles[emotion.label]);
+      const asset = Asset.fromModule(ConversacionD[emotion.label]);
       await asset.downloadAsync();
       const content = await FileSystem.readAsStringAsync(asset.localUri);
       setEmotionInfo(content);
@@ -51,7 +33,7 @@ export function Conversacion() {
     <View style={{ flex: 1, paddingBottom: insets.bottom, backgroundColor: "#FFF8E8" }}>
       <StatusBar style="dark" />
       <View style={{ alignItems: 'center' }}>
-        <Text style={styles.Title}>Me siento</Text>
+        <Text style={styles.Title}>Quiero hablar de</Text>
       </View>
       {selectedEmotion ? (
         // Si hay una emoción seleccionada, muestra su información
